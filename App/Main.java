16ab8f74a1c968412149b62caa0ec173fc1d68b0
Yuri <ytcarvalho14@gmail.com>
2025-03-21 16:59:06 -0300

    Close Scanner object

--- App/Main.java
+++ App/Main.java
@@ -4,37 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-		try (Scanner input = new Scanner(System.in)) {
-			String firstName;
-			String lastName;
-			String occupation;
-			int age;
+		Scanner input = new Scanner(System.in);
 
-			System.out.print("\nWhat is your first name? ");
-			firstName = input.nextLine();
+		String firstName;
+		String lastName;
+		String occupation;
+		int age;
 
-			System.out.print("What is your last name? ");
-			lastName = input.nextLine();
+		System.out.print("\nWhat is your first name? ");
+		firstName = input.nextLine();
 
-			System.out.print("What is your occupation? ");
-			occupation = input.nextLine();
+		System.out.print("What is your last name? ");
+		lastName = input.nextLine();
 
-			System.out.print("How old are you, " + firstName + " " + lastName + "? ");
-			age = input.nextByte();
+		System.out.print("What is your occupation? ");
+		occupation = input.nextLine();
 
-			Person myPerson1 = new Person(firstName, lastName, occupation, age);
-			myPerson1.presentInfo();
+		System.out.print("How old are you, " + firstName + " " + lastName + "? ");
+		age = input.nextByte();
 
-			Person myPerson2 = new Person();
+		input.close();
 
-			myPerson2.setFirstName("Ronaldo");
-			myPerson2.setLastName("Ribeiro");
-			myPerson2.setOccupation("Student");
-			myPerson2.setAge(19);
+		Person myPerson1 = new Person(firstName, lastName, occupation, age);
+		myPerson1.presentInfo();
 
-			myPerson2.presentInfo();
+		Person myPerson2 = new Person();
 
-		}
+		myPerson2.setFirstName("Ronaldo");
+		myPerson2.setLastName("Ribeiro");
+		myPerson2.setOccupation("Student");
+		myPerson2.setAge(19);
+
+		myPerson2.presentInfo();
 
     }
 
